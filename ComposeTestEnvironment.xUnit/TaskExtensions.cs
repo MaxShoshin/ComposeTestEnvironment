cbc95df653dd40e7b530bbdfb3422f87a365986d
Maxim Shoshin <maksim.shoshin@monopoly.su>
2020-12-03 23:42:30 +0300

    Remove unnecessary code

--- ComposeTestEnvironment.xUnit/TaskExtensions.cs
+++ ComposeTestEnvironment.xUnit/TaskExtensions.cs
@@ -14,14 +14,6 @@ namespace ComposeTestEnvironment.xUnit
             }
         }
 
-        public static async Task WithTimeout(this Task task, TimeSpan timeout)
-        {
-            using (var cancellationSource = new CancellationTokenSource(timeout))
-            {
-                await task.WithCancellation(cancellationSource.Token);
-            }
-        }
-
         public static async Task<T> WithCancellation<T>(this Task<T> task, CancellationToken cancellationToken)
         {
             var tcs = new TaskCompletionSource<byte>(TaskCreationOptions.RunContinuationsAsynchronously);
@@ -38,25 +30,6 @@ namespace ComposeTestEnvironment.xUnit
                 }
 
                 return await task;
-            }
-        }
-
-        public static async Task WithCancellation(this Task task, CancellationToken cancellationToken)
-        {
-            var tcs = new TaskCompletionSource<byte>(TaskCreationOptions.RunContinuationsAsynchronously);
-
-            // This disposes the registration as soon as one of the tasks trigger
-            using (cancellationToken.Register(state => { ((TaskCompletionSource<byte>)state!).TrySetResult(1); }, tcs))
-            {
-                var resultTask = await Task.WhenAny(task, tcs.Task);
-
-                if (resultTask == tcs.Task)
-                {
-                    // Operation cancelled
-                    throw new OperationCanceledException(cancellationToken);
-                }
-
-                await task;
             }
         }
     }
